f6fc9d32a2b3c4329acece5b119fc4b0946d0606
cos <betty901019@gmail.com>
2023-06-13 00:38:17 +0800

    initial

---
+++ sample_project4_LLVMIR/test2.s
@@ -0,0 +1,17 @@
+	.text
+	.file	"test2.ll"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	movl	$0, -8(%rsp)
+	movl	$223, -4(%rsp)
+	xorl	%eax, %eax
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.section	".note.GNU-stack","",@progbits
